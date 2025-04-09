565071b12224e73830639d023bff12810cce6487
Favour1692 <akpanfayvhor@gmail.com>
2025-04-09 19:07:59 +0100

    about and contact pages

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,7 +1,134 @@
 import React from "react";
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import CountUp from "react-countup";
+import aboutImg from "./Images/About/aboutImg.jpg";
+import ourstory from "./Images/About/ourstory.jpg";
+import ourstory2 from "./Images/About/ourstory2.jpg";
+import ourtarget from "./Images/About/ourtarget.jpg";
+import expert1 from "./Images/About/expert1.jpg";
+import expert2 from "./Images/About/expert2.jpg";
+import expert3 from "./Images/About/expert3.jpg";
+import expert4 from "./Images/About/expert4.jpg";
 
 const About = () => {
-  return <div>About</div>;
+  useEffect(() => {
+    AOS.init({ duration: 500 });
+  }, []);
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, []);
+
+  const bgStyle = {
+    backgroundImage: `url(${aboutImg})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+  };
+  return (
+    <>
+      <div className="mt-[10vh] text-black">
+        <div className="w-full h-[20vh]" style={bgStyle} data-aos="fade-down">
+          <div className="w-full h-full bg-black/50 flex justify-center items-center">
+            <h2 className="text-5xl text-white" data-aos="fade-down" data-aos-delay="200">
+              About Us
+            </h2>
+          </div>
+        </div>
+        <div className="container">
+          {/* our story */}
+          <div className="grid grid-cols-1 md:grid-cols-2 md:text-left py-8 gap-4">
+            <div className="flex flex-col gap-5 justify-center">
+              <h3 className="text-center md:text-left" data-aos="fade-right">
+                Our Story
+              </h3>
+              <p data-aos="fade-up" data-aos-delay="100">
+                Our journey began with a simple love for greenery and a passion for bringing nature into everyday spaces. With our own farm, we carefully grow and nurture each plant to ensure the highest quality and freshness. We believe that
+                everyone deserves the joy of beautiful, thriving plants, so we set out to make them accessible, affordable, and easy to care for. Today, we take pride in helping homes and offices come to life with vibrant greenery, delivering
+                happiness one plant at a time!
+              </p>
+            </div>
+            <div className="flex justify-center">
+              <div className="flex flex-col md:flex-row items-center justify-center gap-8 w-full pr-4">
+                <img src={ourstory} alt="" className="max-w-[60%] h-[300px] rounded-2xl" data-aos="zoom-in-left" />
+                <img src={ourstory2} alt="" className="max-w-[70%] md:hidden lg:block h-[300px] rounded-2xl" data-aos="zoom-in-left" data-aos-delay="100" />
+              </div>
+            </div>
+          </div>
+          {/* our target */}
+          <div className="grid grid-cols-1 md:grid-cols-2 py-8 gap-4">
+            <div className="order-2 md:order-1 flex justify-center">
+              <img src={ourtarget} alt="" className="max-w-[80%] rounded-2xl" data-aos="zoom-in-right" />
+            </div>
+            <div className="order-1 md:order-2 flex flex-col gap-5 justify-center">
+              <h3 className="text-center md:text-left" data-aos="fade-left">
+                Our Target
+              </h3>
+              <p data-aos="fade-up" data-aos-delay="100">
+                Our goal is to make it easy for everyone to enjoy the beauty and benefits of potted plants, whether at home, in the office, or any indoor space. We cater to both plant lovers and beginners, offering a variety of easy-to-care-for,
+                high-quality plants at affordable prices. With our own farm, we ensure that every plant is nurtured with care before reaching your doorstep. By providing swift delivery and excellent customer support, we ensure a seamless experience
+                from selection to care, helping our customers bring nature closer to their lives.
+              </p>
+            </div>
+          </div>
+          {/* success story */}
+          <div>
+            <h3 data-aos="fade-right">Our proven success</h3>
+            <div className="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 max-w-[80%] mx-auto gap-4">
+              <div className="flex flex-col items-center">
+                <h3 className="font-subheadings tracking-wider">Happy customers</h3>
+                <div className="text-xl md:text-2xl text-brown">
+                  <CountUp end={500} duration={5} enableScrollSpy={true} scrollSpyOnce={false} scrollSpyDelay={1000} />+
+                </div>
+              </div>
+              <div className="flex flex-col items-center">
+                <h3 className="font-subheadings tracking-wider">Plants delivered</h3>
+                <div className="text-xl md:text-2xl text-brown">
+                  <CountUp end={1000} duration={5} enableScrollSpy={true} scrollSpyOnce={false} scrollSpyDelay={1000} />+
+                </div>
+              </div>
+              <div className="flex flex-col items-center">
+                <h3 className="font-subheadings tracking-wider">Plant varieties</h3>
+                <div className="text-xl md:text-2xl text-brown">
+                  <CountUp end={200} duration={5} enableScrollSpy={true} scrollSpyOnce={false} scrollSpyDelay={1000} />+
+                </div>
+              </div>
+              <div className="flex flex-col items-center">
+                <h3 className="font-subheadings tracking-wider">Repeat Customers</h3>
+                <div className="text-xl md:text-2xl text-brown">
+                  <CountUp end={300} duration={5} enableScrollSpy={true} scrollSpyOnce={false} scrollSpyDelay={1000} />+
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Experts */}
+          <div className="py-12">
+            <h3 data-aos="fade-right">Meet our experts</h3>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-8">
+              <div className="flex flex-col items-center max-w-[100%] overflow-hidden mx-auto relative" data-aos="flip-down">
+                <img src={expert1} alt="" className="max-w-[500px]" />
+                <p className="absolute bottom-5 text-center bg-[#333]/40 text-white p-[5px]">Luke Harrison</p>
+              </div>
+              <div className="flex flex-col items-center max-w-[100%] overflow-hidden mx-auto relative" data-aos="flip-down" data-aos-delay="100">
+                <img src={expert2} alt="" className="max-w-[500px]" />
+                <p className="absolute bottom-5 text-center bg-[#333]/40 text-white p-[5px]">Olivia Bennett</p>
+              </div>
+              <div className="flex flex-col items-center max-w-[100%] overflow-hidden mx-auto relative" data-aos="flip-down" data-aos-delay="200">
+                <img src={expert3} alt="" className="max-w-[500px]" />
+                <p className="absolute bottom-5 text-center bg-[#333]/40 text-white p-[5px]">Sophia Mitchell</p>
+              </div>
+              <div className="flex flex-col items-center max-w-[100%] overflow-hidden mx-auto relative" data-aos="flip-down" data-aos-delay="300">
+                <img src={expert4} alt="" className="max-w-[500px]" />
+                <p className="absolute bottom-5 text-center bg-[#333]/40 text-white p-[5px]">Emily Carter</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default About;
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,7 +1,163 @@
 import React from "react";
+import { useEffect } from "react";
+import contactbanner from "./Images/Contact/contactbanner.jpg";
+import { MdEmail } from "react-icons/md";
+import { FaPhone } from "react-icons/fa";
+import { FaAddressBook } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagramSquare } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import farm1 from "./Images/Contact/farm1.jpg";
+import farm2 from "./Images/Contact/farm2.jpg";
+import showroom from "./Images/Contact/showroom.jpg";
+import showroom2 from "./Images/Contact/showroom2.jpg";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  useEffect(() => {
+    AOS.init({ duration: 500 });
+  }, []);
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, []);
+
+  const bgStyle = {
+    backgroundImage: `url(${contactbanner})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center top",
+    backgroundRepeat: "no-repeat",
+  };
+
+  const cards = [
+    {
+      id: 1,
+      logo: <MdEmail />,
+      title: "Email",
+      detail: "plantsellers@gmail.com",
+      delay: "100",
+    },
+    {
+      id: 2,
+      logo: <FaPhone />,
+      title: "Phone",
+      detail: "+1 023 465 7821",
+      delay: "200",
+    },
+    {
+      id: 3,
+      logo: <FaAddressBook />,
+      title: "Address",
+      detail: "Texas, USA",
+      delay: "300",
+    },
+  ];
+  const socials = [
+    {
+      id: 1,
+      logo: <FaInstagramSquare />,
+      detail: "potted_plants",
+      delay: "100",
+    },
+    {
+      id: 2,
+      logo: <FaSquareXTwitter />,
+      detail: "@pottedPlants",
+      delay: "200",
+    },
+    {
+      id: 3,
+      logo: <FaFacebook />,
+      detail: "Potted plants",
+      delay: "300",
+    },
+  ];
+  return (
+    <div className="mt-[10vh]">
+      <div className="w-full h-[20vh]" style={bgStyle} data-aos="fade-down">
+        <div className="w-full h-full flex justify-center items-center bg-black/40">
+          <h2 className="text-5xl text-white" data-aos="fade-down" data-aos-delay="200">
+            Contact Us
+          </h2>
+        </div>
+      </div>
+      <div className="container">
+        <div>
+          <h3 className="py-4" data-aos="fade-right">
+            Reach out to us today
+          </h3>
+          <p className="py-4" data-aos="fade-up" data-aos-delay="100">
+            Have questions or need help choosing the perfect plant? Reach out to us today—we’re happy to assist you with expert advice, order inquiries, or plant care tips.
+          </p>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 xl:w-[80%] mx-auto">
+            {cards.map((data) => (
+              <div key={data.id} className="flex gap-8 sm:gap-4 py-4 px-8 sm:p-4 sm:bg-[#333] rounded-[10px] sm:text-secondary shadow-xl sm:shadow-none mx-auto sm:mx-0" data-aos="fade-left" data-aos-delay={data.delay}>
+                <div className="flex items-center text-3xl">{data.logo}</div>
+                <div className="flex flex-col ">
+                  <div>{data.title}</div>
+                  <div>{data.detail}</div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="py-8">
+          <h3 className="py-4" data-aos="fade-right">
+            Follow our socials
+          </h3>
+          <p className="py-4" data-aos="fade-up" data-aos-delay="100">
+            Stay connected with us on social media for the latest plant care tips, new arrivals, and exclusive offers. Follow us today and join our growing community of plant lovers.
+          </p>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 xl:w-[80%] mx-auto">
+            {socials.map((data) => (
+              <div key={data.id} className="flex gap-8 sm:gap-4 py-4 px-8 sm:p-4 sm:bg-[#333] rounded-[10px] sm:text-secondary shadow-xl sm:shadow-none mx-auto sm:mx-0" data-aos="fade-left" data-aos-delay={data.delay}>
+                <div className=" text-3xl">{data.logo}</div>
+                <div>{data.detail}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 py-8">
+          <div className="border-2 border-thick rounded-t-xl p-8" data-aos="fade-right">
+            <h3 data-aos="fade-right" data-aos-delay="100">
+              Visit our showroom
+            </h3>
+            <p className="py-4" data-aos="fade-up" data-aos-delay="200">
+              Step into our showroom and explore a beautiful collection of lush, vibrant potted plants in person. Whether you're looking for the perfect plant to take home or just want some inspiration, our friendly team is ready to assist you. See
+              the quality and care that go into every plant we grow, and get expert advice on how to care for them. Visit us today and bring home a piece of nature!
+            </p>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-0">
+              <div className="flex flex-col items-center justify-center">
+                <img src={showroom} alt="" className="max-w-[80%] max-h-[400px] rounded-xl shadow-lg shadow-thick" data-aos="zoom-in" />
+              </div>
+              <div className="flex flex-col items-center">
+                <img src={showroom2} alt="" className="max-w-[80%] max-h-[400px] rounded-xl shadow-lg shadow-thick" data-aos="zoom-in" data-aos-delay="200" />
+              </div>
+            </div>
+          </div>
+          <div className="border-2 border-thick p-8 rounded-b-xl" data-aos="fade-left">
+            <h3 data-aos="fade-right" data-aos-delay="100">
+              Visit our farm
+            </h3>
+            <p className="py-4" data-aos="fade-up" data-aos-delay="200">
+              Experience the beauty of nature firsthand by visiting our farm, where we carefully grow and nurture each plant with love and expertise. Take a tour to see how our plants are cultivated from seedlings to the vibrant greenery that
+              brightens homes and offices. Our team is happy to share insights on plant care and sustainability, making your visit both enjoyable and educational. Come explore our farm and take home a fresh, healthy plant straight from the source!
+              <span className="block">Green Haven Farm 1725 Meadowbrook Lane Springfield, IL 62702</span>
+            </p>
+            <div className="grid grid-cols-1 md:grid-cols-2 mx-auto gap-8 md:gap-0">
+              <div className="flex flex-col items-center">
+                <img src={farm1} alt="" className="max-w-[80%] max-h-[350px] rounded-xl shadow-lg shadow-thick" data-aos="zoom-in" />
+              </div>
+              <div className="flex flex-col items-center">
+                <img src={farm2} alt="" className="max-w-[80%] max-h-[350px] rounded-xl shadow-lg shadow-thick" data-aos="zoom-in" data-aos-delay="200" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
