153b56c8f7a47c1882a42f6e85190b7e26a07883
Favour1692 <akpanfayvhor@gmail.com>
2025-04-11 08:20:00 +0100

    footer and icon

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router";
 import { FaFacebook } from "react-icons/fa";
 import { FaInstagramSquare } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
@@ -10,39 +11,37 @@ const Footer = () => {
         <div>Potted Plants</div>
         <div className="flex flex-col gap-4">
           <h4 className="text-lg font-semibold">Quick Links</h4>
-          <a href="" className="active:text-black md:hover:text-black">
+          <Link to="/about" className="active:text-black md:hover:text-black">
             About
-          </a>
-          <a href="" className="active:text-black md:hover:text-black">
+          </Link>
+          <Link to="/products" className="active:text-black md:hover:text-black">
             Products
-          </a>
-          <a href="" className="active:text-black md:hover:text-black">
+          </Link>
+          <Link to="/contact" className="active:text-black md:hover:text-black">
             Contact
-          </a>
-          <a href="" className="active:text-black md:hover:text-black">
-            New Arrivals
-          </a>
+          </Link>
+          <Link to="/gallery" className="active:text-black md:hover:text-black">
+            Gallery
+          </Link>
         </div>
         <div className="flex flex-col gap-4">
           <h4 className="text-lg font-semibold">Popular</h4>
-          <a href="" className="active:text-black md:hover:text-black">
+          <Link to="/products" className="active:text-black md:hover:text-black">
             Indoor plants
-          </a>
-          <a href="" className="active:text-black md:hover:text-black">
+          </Link>
+          <Link to="/products" className="active:text-black md:hover:text-black">
             Outdoor plants
-          </a>
-          <a href="" className="active:text-black md:hover:text-black">
+          </Link>
+          <Link to="/products" className="active:text-black md:hover:text-black">
             Plant gifts
-          </a>
-          <a href="" className="active:text-black md:hover:text-black">
-            Custom plants
-          </a>
+          </Link>
+          <Link to="/products" className="active:text-black md:hover:text-black">
+            Hanging plants
+          </Link>
         </div>
         <div className="flex flex-col gap-4">
           <h4 className="text-lg font-semibold">Contact us</h4>
-          <a href="" className="active:text-black md:hover:text-black">
-            plantsellers@gmail.com
-          </a>
+          <p>plantsellers@gmail.com</p>
           <p>
             Visit our showroom: <br /> plantsAVe, Texas city, USA
           </p>
